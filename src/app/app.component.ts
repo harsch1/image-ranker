73501615ca4ffe5d28ec3c5d0ccf54468cc04a23
harsch1 <>
2020-02-11 00:46:07 -0500

    blabla`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'image-ranker';
+  name = new FormControl('', Validators.required);
+
+  nameForm = new FormGroup({
+    name: this.name
+  });
+
+  step = 0;
+  nameFormSubmitted = false;
+
+  getErrorMessage(elm: FormControl): string {
+    if (elm.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return 'Unknown error';
+  }
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    if (this.step === 0) {
+      console.log(this.nameForm);
+      if (this.nameForm.valid) {
+        this.nameFormSubmitted = true;
+        this.step++;
+      } else {
+        this.name.markAsTouched();
+      }
+    }
+  }
+
+  prevStep() {
+    this.step--;
+  }
 }
