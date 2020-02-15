bd84822aceab27fc0a92f9fe47c278f6b47d63fa
harsch1 <>
2020-02-14 22:36:55 -0500

    Image previews

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+// @ts-ignore
+import IconsJson from '../assets/icons.json';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('fullSize') modal: TemplateRef<any>;
   title = 'image-ranker';
-  name = new FormControl('', Validators.required);
+  name = new FormControl('' , Validators.required);
+  colNum: number;
+  width: number;
 
+  icons  = [];
   nameForm = new FormGroup({
     name: this.name
   });
 
-  step = 0;
+  step = 1;
   nameFormSubmitted = false;
 
+  constructor(public dialog: MatDialog) {}
+
+
+  ngOnInit(): void {
+    this.icons = IconsJson.icons;
+    this.width = window.innerWidth;
+    this.colNum = this.getColumns(this.width);
+    console.log(this.icons);
+  }
+
+  onResize(event) {
+    this.colNum = this.getColumns(event.target.innerWidth);
+  }
+
+  getColumns(width: number): number {
+    const panelWidth = 250;
+    const paddingWidth = 15;
+    if (Math.ceil(width / panelWidth) * width + 10 * Math.ceil(width / panelWidth) + 108 + 48 > width) {
+      return Math.ceil((width - 108) / panelWidth) - 1;
+    }
+    return Math.ceil((width - 108) / panelWidth);
+  }
   getErrorMessage(elm: FormControl): string {
     if (elm.hasError('required')) {
       return 'You must enter a value';
@@ -31,16 +60,26 @@ export class AppComponent {
   nextStep() {
     if (this.step === 0) {
       console.log(this.nameForm);
-      if (this.nameForm.valid) {
-        this.nameFormSubmitted = true;
-        this.step++;
-      } else {
-        this.name.markAsTouched();
-      }
+      // if (this.nameForm.valid) {
+      //   this.nameFormSubmitted = true;
+      this.step++;
+      // } else {
+      //   this.name.markAsTouched();
+      // }
     }
+  }
+  openFullImage(url: string): void {
+    this.dialog.open(this.modal, {
+      data: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        url
+      }
+    });
   }
 
   prevStep() {
     this.step--;
   }
+
 }
